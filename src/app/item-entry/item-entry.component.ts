3fb65b5e71acb2b7519bb8028b5eba5bdac27512
jmefd9 <jmefd9@mail.umsl.edu>
2021-02-04 12:39:45 -0600

    added search feature

--- src/app/item-entry/item-entry.component.ts
+++ src/app/item-entry/item-entry.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { ItemEntryService } from '../item-entry.service';
 import { ItemEntry } from '../itemEntry';
-
 
 @Component({
   selector: 'app-item-entry',
@@ -11,6 +10,8 @@ import { ItemEntry } from '../itemEntry';
 export class ItemEntryComponent implements OnInit {
   
   itemEntries: ItemEntry[];
+  itemName: string;
+
   constructor(private itemEntryService: ItemEntryService) { }
 
   ngOnInit(): void {
@@ -20,4 +21,13 @@ export class ItemEntryComponent implements OnInit {
     });
   }
 
+  Search() {
+    if(this.itemName != ""){
+      this.itemEntries = this.itemEntries.filter(res => {
+        return res.itemName.toLocaleLowerCase().match(this.itemName.toLocaleLowerCase());
+      });
+    }else if(this.itemName == ""){
+      this.ngOnInit();
+    }
+  }
 }
